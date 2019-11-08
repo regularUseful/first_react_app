d555d2e2e2e684b4f0cb32cd03e6f32682cdced2
scatterbrain3 <scattini3@hotmail.com>
2019-11-08 14:02:25 +1030

    created board

--- src/App.js
+++ src/App.js
@@ -2,6 +2,27 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+class Cell extends React.Component{
+  constructor(props){
+    super(props);
+    this.handleClick = this.handleClick.bind(this)
+  }
+
+  handleClick(evt){
+    this.props.flipCellsAroundMe();
+  }
+
+  render(){
+    let classes = "Cell " + (this.props.isLit ? "Cell-lit" : "");
+
+    return(
+      <td className={classes} onClick={this.handleClick} />
+    )
+
+  }
+
+}
+
 
 class Board extends React.Component{
 
@@ -19,6 +40,20 @@ class Board extends React.Component{
     }
 
     this.createBoard = this.createBoard.bind(this);
+    this.flipCellsAround = this.flipCellsAround.bind(this);
+  }
+
+  flipCellsAround(coord){
+    let {ncols, nrows, } = this.props;
+    let board = this.state.board;
+    let [y , x] = coord.split("-").map(Number);
+
+    function flipCell(y, x){
+      if( x >=  0 && x < ncols && y >= 0 && y < nrows){
+        board[y][x] = !board[y][x];
+      }
+    }
+
   }
 
   createBoard(){
@@ -34,7 +69,23 @@ class Board extends React.Component{
   }
 
   render(){
-    return(<h1>hi</h1>)
+
+    let tblBoard = [];
+    for(let y = 0; y < this.props.nrows; y++){
+      let row = [];
+      for(let x = 0; x < this.props.ncols; x++){
+        let coord = `${y}-${x}`
+        row.push(<Cell key = {coord} isLit = {this.state.board[y][x]} />)
+      }
+      tblBoard.push(<tr key={y}>{row}</tr>)
+    }
+    return(
+      <table className = "Board">
+        <tbody>
+          {tblBoard}
+        </tbody>
+      </table>
+    )
   }
 
 }
