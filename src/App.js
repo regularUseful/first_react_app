b15a77bbab9f05cce8a44292bbf1844ee2049ea6
scatterbrain3 <scattini3@hotmail.com>
2019-11-08 12:14:21 +1030

    added the create board array

--- src/App.js
+++ src/App.js
@@ -2,25 +2,53 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+
+class Board extends React.Component{
+
+  static defaultProps = {
+    nrows: 5,
+    ncols: 5,
+    chanceLightStartsOn: 0.25
+  }
+
+  constructor(props){
+    super(props)
+    this.state = {
+      hasWon: false,
+      board: this.createBoard()
+    }
+
+    this.createBoard = this.createBoard.bind(this);
+  }
+
+  createBoard(){
+    let board = [];
+    for(let y = 0; y < this.props.nrows; y++){
+      let row = [];
+      for(let x = 0; x < this.props.ncols; x++){
+        row.push(Math.random() < this.props.chanceLightStartsOn)
+      }
+      board.push(row);
+    }
+    return board;
+  }
+
+  render(){
+    return(<h1>hi</h1>)
+  }
+
+}
+
+
+
 class App extends React.Component {
+  
+
   render(){
 
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+       <Board />
       </div>
     );
   }
